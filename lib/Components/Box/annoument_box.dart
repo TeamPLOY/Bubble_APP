6d230fe567f64b63480b9a872b3e185260222a88
soul071213 <js071213asd@gmail.com>
2024-09-12 19:28:44 +0900

    feat : 공지사항 컴포넌트 개발 완료

---
+++ lib/Components/Box/annoument_box.dart
@@ -0,0 +1,28 @@
+import 'package:bubble_app/theme.dart';
+import 'package:flutter/material.dart';
+
+class AnnoumentBox extends StatelessWidget {
+  AnnoumentBox({required this.text,required this.date,super.key});
+  final String text;
+  final String date;
+  late String ymd=date.substring(0,3)+'.'+date.substring(4,5)+'.'+date.substring(6,7);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * (345 / 393),
+      height: 84,
+      child: Padding(
+        padding: EdgeInsets.only(left: 17,top: 27),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('${text}',style: semiBold14.copyWith(color: gray800),),
+            SizedBox(height: 5,),
+            Text('${ymd}',style: medium12.copyWith(color:gray600),)
+          ],
+        ),
+      ),
+    );
+  }
+}
