8588fa09ff9d04f26bc299f305f963b53a28d8ee
yunse0708 <yunse070708@gmail.com>
2024-09-12 16:47:25 +0900

    feat : 개발 환경 추가

---
+++ lib/theme.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+// White Color
+const white100 = Color(0xffffffff);
+
+// Red Color
+const red100 = Color(0xFFF05A5A);
+const red200 = Color(0xffFF4444);
+const red300 = Color(0xffFF0000);
+
+// Blue Color
+const blue0 = Color(0xffDCE3FF);
+const blue100 = Color(0xffC8D4FF);
+const blue200 = Color(0xffA7BAFF);
+const blue300 = Color(0xff7996FF);
+const blue400 = Color(0xff4970FB);
+const blue500 = Color(0xff0037FF);
+const blue600 = Color(0xff365BE0);
+const blue700 = Color(0xff1F42C1);
+const blue800 = Color(0xff263D8E);
+const blue900 = Color(0xff0A1A57);
+
+// Gray Color
+const gray100 = Color(0xffF8F9FA);
+const gray200 = Color(0xffF2F5F7);
+const gray300 = Color(0xFfDEE2E6);
+const gray400 = Color(0xFFCED4DA);
+const gray500 = Color(0xFFADB5BD);
+const gray600 = Color(0xFF6B7280);
+const gray700 = Color(0xFF495057);
+const gray800 = Color(0xFF343A40);
+
+// Regular
+const regular12 = TextStyle(
+    fontFamily: 'PretendardRegular', fontSize: 12, fontWeight: FontWeight.w400);
+
+const regular14 = TextStyle(
+    fontFamily: 'PretendardRegular', fontSize: 14, fontWeight: FontWeight.w400);
+
+// Medium
+const medium8 = TextStyle(
+    fontFamily: 'PretendardMedium', fontSize: 8, fontWeight: FontWeight.w500);
+
+const medium10 = TextStyle(
+    fontFamily: 'PretendardMedium', fontSize: 10, fontWeight: FontWeight.w500);
+
+const medium12 = TextStyle(
+    fontFamily: 'PretendardMedium', fontSize: 12, fontWeight: FontWeight.w500);
+
+const medium14 = TextStyle(
+    fontFamily: 'PretendardMedium', fontSize: 14, fontWeight: FontWeight.w500);
+
+const medium16 = TextStyle(
+    fontFamily: 'PretendardMedium', fontSize: 16, fontWeight: FontWeight.w500);
+
+// Semi-Bold
+const semiBold10 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 10,
+    fontWeight: FontWeight.w600);
+
+const semiBold12 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 12,
+    fontWeight: FontWeight.w600);
+const semiBold14 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 14,
+    fontWeight: FontWeight.w600);
+
+const semiBold16 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 16,
+    fontWeight: FontWeight.w600);
+
+const semiBold24 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 24,
+    fontWeight: FontWeight.w600);
+
+// Bold
+const bold14 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 14,
+    fontWeight: FontWeight.w700);
+const bold16 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 16,
+    fontWeight: FontWeight.w700);
+const bold20 = TextStyle(
+    fontFamily: 'PretendardSemiBold',
+    fontSize: 20,
+    fontWeight: FontWeight.w700);
