7be6bcaf73b193396e88082e584e4bfc443de2ac
yunse0708 <yunse070708@gmail.com>
2024-09-15 11:00:23 +0900

    feat : 예약 페이지 전환

--- lib/Components/Button/reservationButton.dart
+++ lib/Components/Button/reservationButton.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:bubble_app/theme.dart';
 
 class Reservationbutton extends StatelessWidget {
-  const Reservationbutton({super.key});
+  final VoidCallback onPressed;
+
+  const Reservationbutton({super.key, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width - 48,
-      height: 44,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30), color: blue400),
-      child: Center(
-        child: Text(
-          "예약하기",
-          style: bold16.copyWith(color: white100),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: MediaQuery.of(context).size.width - 48,
+        height: 44,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: blue400,
+        ),
+        child: Center(
+          child: Text(
+            "예약하기",
+            style: bold16.copyWith(color: white100),
+          ),
         ),
       ),
     );
