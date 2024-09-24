4f7a93f01ebe6ee2e6a634a9fa72cc55f621e0f4
soul071213 <js071213asd@gmail.com>
2024-09-24 19:51:53 +0900

    feat : first page 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:bubble_app/Components/Button/login.dart';
-import 'package:bubble_app/Pages/my_page.dart';
+
+import 'package:bubble_app/Pages/first_page.dart';
 import 'package:flutter/material.dart';
-import 'package:bubble_app/Pages/login_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: FirstPage(),
     );
   }
 }
