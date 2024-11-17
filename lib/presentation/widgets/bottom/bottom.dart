56f076214196229016567573a280b1f639ccda3c
soul071213 <127070775+soul071213@users.noreply.github.com>
2024-11-17 22:10:55 +0900

    bottom 변경

--- lib/presentation/widgets/bottom/bottom.dart
+++ lib/presentation/widgets/bottom/bottom.dart
@@ -50,7 +50,7 @@ class _BottomState extends State<Bottom> {
                 setState(() {
                   get_user_state();
                 });
-                if (check_user == false) {
+                if (check_user == true) {
                   showDialog(
                       context: context,
                       builder: (BuildContext context) {
