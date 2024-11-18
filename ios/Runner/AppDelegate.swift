8e5d154c4244625a5b057703dd1cb35a4f232711
yunse0708 <yunse070708@gmail.com>
2024-11-18 17:39:34 +0900

    fix : 스타일 수정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,34 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    // APNs 등록
+    UIApplication.shared.registerForRemoteNotifications()
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+    print("Device Token: \(token)")
+    
+    // 서버에 토큰 전송
+    sendDeviceTokenToServer(token: token)
+  }
+
+  // APNs 등록 실패 시 호출
+  override func application(
+    _ application: UIApplication,
+    didFailToRegisterForRemoteNotificationsWithError error: Error
+  ) {
+    print("Failed to register for remote notifications: \(error)")
+  }
+
+  private func sendDeviceTokenToServer(token: String) {
+    // 서버로 토큰 전송 로직 작성 (예: HTTP 요청)
+    print("Sending device token to server: \(token)")
   }
 }
