bbfcd24f08707f971b1f0c9acb5ec1019f19207f
soul071213 <js071213asd@gmail.com>
2024-11-21 01:01:39 +0900

    fix : finish 아이콘 수정

--- lib/presentation/pages/deleteUser/delete_next_page.dart
+++ lib/presentation/pages/deleteUser/delete_next_page.dart
@@ -26,7 +26,7 @@ class DeleteNextPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SvgPicture.asset(
-                  'assets/img/checkicon.svg',
+                  'assets/img/finish.svg',
                   width: MediaQuery.of(context).size.width *(142/393),
                   height: MediaQuery.of(context).size.width  *(142/393),
                   ),
